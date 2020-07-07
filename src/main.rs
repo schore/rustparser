b61a50b568238f60ed8b5035003140c5143f10a6
schore <schore.r@gmx.de>
2020-07-07 17:54:12 +0200

    Now with or statement in action

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
 struct ParserOutput<A: Clone>(Option<(A, String)>);
-struct Parser<A: Clone, F>(F)
-where
-    F: Fn(&String) -> ParserOutput<A>;
+struct Parser<A: Clone>(Box<dyn Fn(&String)->ParserOutput<A>>);
 
 impl<A: Clone> ParserOutput<A> {
     fn only_if<F>(&self, f: F) -> ParserOutput<A>
@@ -33,9 +31,7 @@ impl<A: Clone> ParserOutput<A> {
         }
     }
 
-    fn and_then<B: Clone, F>(&self, parser: &Parser<B, F>) -> ParserOutput<B>
-    where
-        F: Fn(&String) -> ParserOutput<B>,
+    fn and_then<B: Clone>(&self, parser: &Parser<B>) -> ParserOutput<B>
     {
         match &self.0 {
             Some((_, s)) => parser.parse(&s),
@@ -44,26 +40,34 @@ impl<A: Clone> ParserOutput<A> {
     }
 }
 
-impl<A: Clone, F> Parser<A, F>
-where
-    F: Fn(&String) -> ParserOutput<A>,
+impl<A: Clone> Parser<A>
+// where
+    // F: Fn(&String) -> ParserOutput<A>,
 {
+    fn new<F>(f: F) -> Parser<A>
+    where F: 'static + Fn(&String) -> ParserOutput<A>
+    {
+        Parser(Box::new(f))
+    }
+
     fn parse(&self, input: &String) -> ParserOutput<A> {
         (self.0)(input)
     }
+
+    fn or(self, p2 :Parser<A>) -> Parser<A>
+    where A : 'static
+    {
+        Parser::new(move |input : &String| {
+            let result = self.parse(input);
+            if result.0.is_some() {
+                return result;
+            }
+            return p2.parse(input);
+        })
+    }
+
 }
 
-fn or<A: Clone, F1, F2>(input: &String, p1: &Parser<A, F1>, p2: &Parser<A, F2>) -> ParserOutput<A>
-where
-    F1: Fn(&String) -> ParserOutput<A>,
-    F2: Fn(&String) -> ParserOutput<A>,
-{
-    let result1 = p1.parse(input);
-    match &result1.0 {
-        Some(_) => result1,
-        None => p2.parse(input),
-    }
-}
 
 fn item(input: &String) -> ParserOutput<char> {
     let item = input.chars().next();
@@ -87,9 +91,7 @@ fn is_alphabetic<'a>(input: &String) -> ParserOutput<char> {
     item(input).only_if(|c| c.is_alphabetic())
 }
 
-fn get_all<A: Clone, F>(input: &String, parser: &Parser<A, F>) -> ParserOutput<Vec<A>>
-where
-    F: Fn(&String) -> ParserOutput<A>,
+fn get_all<A: Clone>(input: &String, parser: &Parser<A>) -> ParserOutput<Vec<A>>
 {
     let mut ret: Vec<A> = Vec::new();
     let mut rest = input.clone();
@@ -105,15 +107,14 @@ where
     ParserOutput(Some((ret, rest)))
 }
 
-fn at_least_one<A: Clone, F>(input: &String, parser: &Parser<A, F>) -> ParserOutput<Vec<A>>
+fn at_least_one<A: Clone>(input: &String, parser: &Parser<A>) -> ParserOutput<Vec<A>>
 where
-    F: Fn(&String) -> ParserOutput<A>,
 {
     get_all(input, parser).only_if(|v| v.len() > 0)
 }
 
 fn word(input: &String) -> ParserOutput<String> {
-    at_least_one(input, &Parser(is_alphabetic)).map(|r| r.into_iter().collect())
+    at_least_one(input, &Parser::new(is_alphabetic)).map(|r| r.into_iter().collect())
 }
 
 fn white_space(input: &String) -> ParserOutput<char> {
@@ -121,7 +122,7 @@ fn white_space(input: &String) -> ParserOutput<char> {
 }
 
 fn clear_white_space(input: &String) -> ParserOutput<()> {
-    get_all(input, &Parser(white_space)).map(|_| ())
+    get_all(input, &Parser::new(white_space)).map(|_| ())
 }
 
 fn keyword(input: &String, keyword: &str) -> ParserOutput<()> {
@@ -130,39 +131,39 @@ fn keyword(input: &String, keyword: &str) -> ParserOutput<()> {
 
 #[derive(Clone, Debug)]
 enum Action {
-    doAction(String),
-    entry(String),
-    exit(String)
+    DoAction(String),
+    Entry(String),
+    Exit(String)
 }
 
-fn _action(input: &String, action: &str) -> ParserOutput<String> {
+fn _action(input: &String, action: &'static str) -> ParserOutput<String> {
     clear_white_space(input)
-        .and_then(&Parser(|x| keyword(x, action)))
-        .and_then(&Parser(clear_white_space))
-        .and_then(&Parser(word))
+        .and_then(&Parser::new(move |x| keyword(x, action)))
+        .and_then(&Parser::new(clear_white_space))
+        .and_then(&Parser::new(word))
 }
 
-fn doAction(input: &String) -> ParserOutput<Action> {
+fn do_action(input: &String) -> ParserOutput<Action> {
     _action(input, "do")
-        .map(|s| Action::doAction(s.clone()))
+        .map(|s| Action::DoAction(s.clone()))
 }
 
-fn entryAction(input: &String) -> ParserOutput<Action> {
+fn entry_action(input: &String) -> ParserOutput<Action> {
     _action(input, "entry")
-        .map(|s| Action::entry(s.clone()))
+        .map(|s| Action::Entry(s.clone()))
 }
 
-fn exitAction(input: &String) -> ParserOutput<Action> {
+fn exit_action(input: &String) -> ParserOutput<Action> {
     _action(input, "exit")
-        .map(|s| Action::exit(s.clone()))
+        .map(|s| Action::Exit(s.clone()))
 }
 
-// fn action(input: &String) -> ParserOutput<Action> {
-//     let actionDo = Parser(doAction);
-//     let actionEntry = Parser(entryAction);
-//     let actionExit = Parser(exitAction);
-// }
-
+fn action(input: &String) -> ParserOutput<Action> {
+    Parser::new(&do_action)
+        .or(Parser::new(&entry_action))
+        .or(Parser::new(&exit_action))
+        .parse(input)
+}
 
 fn main() {
     println!("{:#?}", item(&"Foo".to_string()));
@@ -174,12 +175,12 @@ fn main() {
     println!("{:#?}", is_numeric(&"a".to_string()));
     println!("{:#?}", is_numeric(&"01".to_string()));
 
-    println!("{:#?}", get_all(&"01f".to_string(), &Parser(is_numeric)));
-    println!("{:#?}", at_least_one(&"".to_string(), &Parser(item)));
+    println!("{:#?}", get_all(&"01f".to_string(), &Parser::new(is_numeric)));
+    println!("{:#?}", at_least_one(&"".to_string(), &Parser::new(item)));
     // println!("{:#?}", at_least_one("1", &item));
     println!(
         "{:#?}",
-        at_least_one(&"12f".to_string(), &Parser(is_numeric))
+        at_least_one(&"12f".to_string(), &Parser::new(is_numeric))
     );
 
     println!("{:#?}", word(&"Als ich".to_string()));
@@ -187,5 +188,5 @@ fn main() {
 
     println!("{:#?}", clear_white_space(&"  aba".to_string()));
     println!("{:#?}", keyword(&"Foo Bar".to_string(), "Foo"));
-    println!("{:#?}", doAction(&" do Namaear bla".to_string()));
+    println!("{:#?}", action(&" entry Namaear bla".to_string()));
 }
